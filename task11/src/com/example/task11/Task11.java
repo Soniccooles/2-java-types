38e2d36845688b1208a228f9d75f3b70d5ff59df
Soniccooles <andrey.sonic@mail.ru>
2024-09-14 22:55:45 +0500

    Сакулин, модуль 2 задание 11

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -1,27 +1,21 @@
 package com.example.task11;
 
-public class Task11 {
-
-    public static float benefit(float sum, float percent) {
-
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+public class Task11
+{
+    public static float benefit(float sum, float percent)
+    {
+        System.out.println((float)(sum*(1 + percent * 12.0)));
+        return (float)(sum*Math.pow(1.0 + percent, 12.0));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        float sum = 500; // 500 руб. на счете
+        float sum = 500.000000000f; //   500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
         sum = benefit(sum, percent);
 
-        System.out.println("Сумма на счете через год: " + sum);
-
+        System.out.printf("Сумма на счете через год: %.10f%n", sum);
     }
-
 }
